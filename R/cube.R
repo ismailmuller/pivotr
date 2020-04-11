123237f24f46e719da1dadb0d10370f7ac12c89e
ismailmuller <muller.ismail@gmail.com>
2020-04-11 22:15:39 +0200

    Initializing the package

---
+++ R/cube.R
@@ -0,0 +1,40 @@
+#' Computes grouped aggregates
+#'
+#' @description Calculates aggregates functions for groups of variables and all their combinations
+#'
+#' @param df A dataframe
+#' @param groups variables names
+#' @param ... calculations passed to dplyr::summarize()
+#' @param .totals = "Total"
+#' 
+#' @export
+#' 
+#' @examples
+#' cube(mtcars, c(cyl, am), Avg = mean(mpg))
+
+cube <- function(df, groups, ..., .totals = "Total"){
+  groups <- rlang::enquos(groups)
+  rnames <- dplyr::select(df, !!!groups) %>% names()
+  calculations <- rlang::enquos(...)
+  
+  add_rnames_columns <- function(df, total_replacement = .totals){
+    cols_to_add <- setdiff(rnames, names(df))
+    if(length(cols_to_add) > 0 & !is.na(.totals)){
+      for( r in cols_to_add ){
+        df <- mutate(df, !! r := total_replacement)
+      }}
+    return(df)
+  }
+
+  combinations <- purrr::map(c(0, seq_along(rnames)), ~ combn(rnames, .x) %>% as.data.frame(stringsAsFactors = FALSE) )
+  purrr::map(combinations,  
+             ~ purrr::map(.x, ~ dplyr::group_by_at(df, .x) %>%
+                            dplyr::summarize( !!!calculations ) %>%
+                            dplyr::ungroup() %>% 
+                            dplyr::mutate_at(tidyselect::all_of(.x), as.character) %>% 
+                            add_rnames_columns()) %>% 
+               dplyr::bind_rows()) %>%
+    dplyr::bind_rows() %>%
+    dplyr::select_at( dplyr::vars( tidyselect::all_of(rnames), dplyr::everything() ) ) %>%
+    dplyr::arrange_at( tidyselect::all_of(rnames) )
+}
---
+++ R/pivot_table.R
@@ -0,0 +1,62 @@
+#' Pivot table
+#' 
+#' @description Calculates aggregates in pivot tables
+#' 
+#' @author Ismail Muller
+#'
+#' @param df A dataframe
+#' @param rows A vector of variable names that will be on the rows of the pivot table
+#' @param cols A vector of variable names that will be on the columns of the pivot table
+#' @param ... calculations that are passed to `dplyr::summarise()` via the `cube()` function
+#' 
+#' @return a tibble
+#' 
+#' @export
+#' 
+#' @examples 
+#' 
+#' pvt(mtcars, cyl, am, N = n(), Avg = mean(mpg))
+#' pvt(mtcars, cyl, am, Avg = mean(wt), N = n())
+#' pvt(mtcars, NULL, am, Avg = mean(wt))
+#' pvt(mtcars, c(vs,am), cyl, N = n(), .keep_total = "overall")
+
+pvt <- function(df, rows = NULL, cols = NULL, ..., .totals = "Total", .keep_total = "all"){
+  calculations <- rlang::enquos(...)
+  rows <- rlang::enquos(rows)
+  cols <- rlang::enquos(cols)
+  rnames <- dplyr::select(df, !!!rows ) %>% names()
+  cnames <- dplyr::select(df, !!!cols) %>% names()
+  groups <- c(rnames, cnames)
+  groups <- groups[!is.null(groups)]
+  
+  the_cube <- cube(df, groups = tidyselect::all_of(groups), !!!calculations, .totals = .totals)
+  calcnames <- setdiff(names(the_cube), c(rnames, cnames))
+  
+  # pivot only if cols is not null
+  if(length(cnames) > 0){
+    res <- tidyr::pivot_wider(
+      the_cube,
+      id_cols = tidyselect::all_of(rnames),
+      names_from = tidyselect::all_of(cnames),
+      values_from = tidyselect::all_of(calcnames))
+  } else {
+    res <- the_cube
+  }
+  
+  # arrange the rows
+  res <- res %>% 
+    dplyr::mutate_at(tidyselect::all_of(rnames), as.character) %>% 
+    dplyr::arrange_at(tidyselect::all_of(rnames), 
+                      .funs = list(function(x){!is.na(x) & x == "Total"} ))
+  
+  # keep only the wanted totals
+  res <- switch(.keep_total,
+                "all" = res,
+                "overall" = dplyr::bind_rows(dplyr::filter_at(res, tidyselect::all_of(rnames), ~ .x != .totals | is.na(.x)),
+                                             dplyr::slice(res, n())),
+                "none" =  dplyr::filter_at(res, tidyselect::all_of(rnames), ~ .x != .totals))
+  
+  return(res)
+}
+
+pivot_table <- pvt
